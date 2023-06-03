8935d5c02342f6f258ea6838bad8f3c20deede34
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2023-06-03 17:42:43 +0800

    Update app.py

--- watchDog/app.py
+++ watchDog/app.py
@@ -5,12 +5,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from flask_user import roles_required
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/watchdog'
+# app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 
 login_manager = LoginManager()
@@ -27,6 +28,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    usertype = db.Column(db.Integer, nullable=False)
+    location1 = db.Column(db.Integer, nullable=True)
+    location2 = db.Column(db.Integer, nullable=True)
 
 
 class RegisterForm(FlaskForm):
@@ -75,6 +79,7 @@ def login():
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
+# @roles_required('Admin')
 def dashboard():
     return render_template('dashboard.html')
 
